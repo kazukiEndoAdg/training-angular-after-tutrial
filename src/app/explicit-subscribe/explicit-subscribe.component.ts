a230951e3ce02d8f7816546453f1fb21cefbdb0b
kazuki.endo <kazuki.endo@adglobe.co.jp>
2022-01-17 17:23:02 +0900

    [update]最も簡単に明示的にunsubscribeするタイミングを指定するパターン

--- src/app/explicit-subscribe/explicit-subscribe.component.ts
+++ src/app/explicit-subscribe/explicit-subscribe.component.ts
@@ -1,18 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-explicit-subscribe',
   template: `<div>{{ value }}</div>`,
 })
-export class ExplicitSubscribeComponent implements OnInit {
+export class ExplicitSubscribeComponent implements OnInit, OnDestroy {
   value: any;
-
+  private subscription!: Subscription;
   constructor(private dataService: DataService) {}
 
-  ngOnInit(): void {
-    this.dataService.valueChanges.subscribe((value) => {
+  ngOnInit() {
+    this.subscription = this.dataService.valueChanges.subscribe((value) => {
       this.value = value;
     });
+  }
+
+  ngOnDestroy() {
+    //   コンポーネントが破棄されるタイミングで購読解除されるように
+    //   explicit unsubscribe
+    this.subscription.unsubscribe();
   }
 }
